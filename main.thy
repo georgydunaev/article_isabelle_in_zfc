75a940c0bddbc43258a62520b526996baac5e414
Georgy Dunaev <georgedunaev@gmail.com>
2020-03-29 18:27:22 +0300

    ite1,ite2

--- main.thy
+++ main.thy
@@ -7,7 +7,7 @@ definition fite :: "[i, o, i, i] \<Rightarrow> i" (\<open>from _ if _ then _ els
 definition ite :: "[o, i, i] \<Rightarrow> i" (\<open>myif _ then _ else _\<close>)
   where "ite(\<phi>, a, b) == \<Union>{x\<in>{a,b}.(\<phi>\<and>x=a)\<or>((\<not>\<phi>)\<and>x=b)}"
 
-theorem thm1 : \<open>\<phi> \<Longrightarrow> ((myif \<phi> then a else b) = a)\<close>
+theorem ite1 : \<open>\<phi> \<Longrightarrow> ((myif \<phi> then a else b) = a)\<close>
 proof (unfold ite_def)
   assume H:\<open>\<phi>\<close>
   have P:\<open>{x \<in> {a, b} . \<phi> \<and> x = a \<or> \<not> \<phi> \<and> x = b} = {a}\<close>
@@ -15,10 +15,7 @@ proof (unfold ite_def)
     show \<open> {x \<in> {a, b} . \<phi> \<and> x = a \<or> \<not> \<phi> \<and> x = b} \<subseteq> {a}\<close>
     proof
       fix x
-      assume G:\<open>x \<in>
-         {x \<in> {a, b} .
-          \<phi> \<and> x = a \<or>
-          \<not> \<phi> \<and> x = b}\<close>
+      assume G:\<open>x \<in> {x \<in> {a, b} .  \<phi> \<and> x = a \<or> \<not> \<phi> \<and> x = b}\<close>
       have U:\<open>\<phi> \<and> x = a \<or> \<not> \<phi> \<and> x = b\<close> by (rule CollectE[OF G])
 
       have T:\<open>x = a\<close>
@@ -72,6 +69,69 @@ proof (unfold ite_def)
   (*find_theorems "\<Union>{_}"*)
   (*find_theorems "_=_\<Longrightarrow>_=_ \<Longrightarrow>_"*)
   show \<open>\<Union>{x \<in> {a, b} . \<phi> \<and> x = a \<or> \<not> \<phi> \<and> x = b} = a\<close>
+    by (rule IFOL.trans[OF P1 P2])
+qed
+
+theorem ite2 : \<open>\<not>\<phi> \<Longrightarrow> ((myif \<phi> then a else b) = b)\<close>
+proof (unfold ite_def)
+  assume H:\<open>\<not>\<phi>\<close>
+  have P:\<open>{x \<in> {a, b} . \<phi> \<and> x = a \<or> \<not> \<phi> \<and> x = b} = {b}\<close>
+  proof 
+    show \<open> {x \<in> {a, b} . \<phi> \<and> x = a \<or> \<not> \<phi> \<and> x = b} \<subseteq> {b}\<close>
+    proof
+      fix x
+      assume G:\<open>x \<in> {x \<in> {a, b} .  \<phi> \<and> x = a \<or> \<not> \<phi> \<and> x = b}\<close>
+      have U:\<open>(\<phi> \<and> x = a) \<or> (\<not> \<phi> \<and> x = b)\<close> by (rule CollectE[OF G])
+
+      have T:\<open>x = b\<close>
+      proof (rule disjE[OF U conjunct2])
+        assume \<open>\<not>\<phi> \<and> x = b\<close>
+        then show \<open>x = b\<close> by (rule conjunct2)
+      next
+        assume \<open>\<phi> \<and> x = a\<close>
+        then have Y:\<open>\<phi>\<close> by (rule conjunct1)
+        have T1:\<open>x = b\<close> by (rule notE[OF H Y])
+        show \<open>\<not>\<phi> \<and> x = b\<close> by (rule conjI[OF H T1])
+      qed
+      have R:\<open>x\<in>{x}\<close> by (rule upair.singletonI)
+      find_theorems "_\<in>{_}"
+      show \<open>x\<in>{b}\<close>
+      proof (rule subst)
+        show \<open>x\<in>{x}\<close> by (rule upair.singletonI)
+      next
+        show \<open>{x} = {b}\<close> by (rule IFOL.subst_context[OF T])
+      qed
+    qed
+  next
+    show \<open>{b} \<subseteq> {x \<in> {a, b} . \<phi> \<and> x = a \<or> \<not> \<phi> \<and> x = b}\<close>
+    proof (*(rule subsetI)... Which rule is used here? *)
+      show\<open>b \<in> {x \<in> {a, b} . \<phi> \<and> x = a \<or> \<not> \<phi> \<and> x = b} \<and>
+      0 \<subseteq> {x \<in> {a, b} . \<phi> \<and> x = a \<or> \<not> \<phi> \<and> x = b}\<close>
+      proof
+        show \<open>0 \<subseteq> {x \<in> {a, b} . \<phi> \<and> x = a \<or> \<not> \<phi> \<and> x = b}\<close>
+          by blast
+      next
+        show \<open>b \<in> {x \<in> {a, b} . \<phi> \<and> x = a \<or> \<not> \<phi> \<and> x = b}\<close>
+        proof 
+          show \<open>b \<in> {a, b}\<close> by blast
+        next
+          show \<open>(\<phi> \<and> b = a) \<or> (\<not> \<phi> \<and> b = b)\<close>
+          proof (rule disjI2)
+            show \<open>(\<not>\<phi>) \<and> b = b\<close>
+            proof
+              show \<open>\<not>\<phi>\<close> by (rule H)
+            next
+              show \<open>b = b\<close> by blast
+            qed
+          qed
+        qed
+      qed
+    qed
+  qed
+  have P1:\<open>\<Union>{x \<in> {a, b} . \<phi> \<and> x = a \<or> \<not> \<phi> \<and> x = b} = \<Union>{b}\<close>
+    by (rule IFOL.subst_context[OF P])
+  have P2:\<open>\<Union>{b} = b\<close> by (rule equalities.Union_singleton)
+  show \<open>\<Union>{x \<in> {a, b} . \<phi> \<and> x = a \<or> \<not> \<phi> \<and> x = b} = b\<close>
     by (rule IFOL.trans[OF P1 P2])
 qed
 
